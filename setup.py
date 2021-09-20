5341abdcb89c939778c07c5485d9a759e35f445e
Aart Stuurman <aart@astuurman.com>
2021-09-20 16:26:55 +0200

    Rename package lower case

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="Revolve2",
+    name="revolve2",
     version="0.0.0",
     description="Evolutionary algorithms framework",
     author="Computational Intelligence Group Vrije Universiteit",

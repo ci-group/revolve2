b21e99cc46e27c1b7611f5a4f59f5bb17b4b883e
Aart Stuurman <aart@astuurman.com>
2021-09-29 17:23:52 +0200

    Add py.typed to make package typed

--- setup.py
+++ setup.py
@@ -6,5 +6,12 @@ setup(
     description="Evolutionary algorithms framework",
     author="Computational Intelligence Group Vrije Universiteit",
     url="https://github.com/ci-group/revolve2",
-    packages=["revolve2"],
+    packages=find_packages(),
+    package_data={"revolve2": ["py.typed"]},
+    install_requires=[
+        "networkx>=2.6.3",
+        "numpy>=1.21.2",
+        "rootpath>=0.1.1",
+        "matplotlib>=3.4.3",
+    ],
 )

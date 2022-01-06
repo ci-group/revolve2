066e7a63ac6e39853a7d045b299b4d5732d5a630
Aart Stuurman <aart@astuurman.com>
2022-01-06 13:20:16 +0100

    Add support for multiple runs to plt_ea_fitness script.

--- core/revolve2/core/analysis/plot_ea_fitness.py
+++ core/revolve2/core/analysis/plot_ea_fitness.py
@@ -9,52 +9,82 @@ from statistics import mean
 import matplotlib.pyplot as plt
 from revolve2.core.database.sqlite import Database
 from revolve2.core.optimization.ea import Analyzer as EaAnalyzer
+from revolve2.core.optimization.ea.analyzer import Generation
 
 
 async def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("database")
+    parser.add_argument(
+        "databases",
+        nargs="+",
+        help="The databases to make plots for. If more then one database(multiple runs of the same experiment) is provided, their respective plots are averaged into one single plot.",
+    )
     args = parser.parse_args()
 
-    db = await Database.create(args.database)
-    with db.begin_transaction() as txn:
-        analyzer = EaAnalyzer(txn, db.root)
+    max_fitnesses = []
+    min_fitnesses = []
+    mean_fitnesses = []
 
-        max_fitness = [
-            max(
-                [analyzer.individuals[individual].fitness for individual in generation],
-            )
-            for generation in analyzer.generations
-        ]
+    for db_file in args.databases:
+        db = await Database.create(db_file)
+        with db.begin_transaction() as txn:
+            analyzer = EaAnalyzer(txn, db.root)
 
-        min_fitness = [
-            min(
-                [analyzer.individuals[individual].fitness for individual in generation],
-            )
-            for generation in analyzer.generations
-        ]
+            max_fitness = [
+                max(
+                    [
+                        analyzer.individuals[individual].fitness
+                        for individual in generation
+                    ],
+                )
+                for generation in analyzer.generations
+            ]
 
-        mean_fitness = [
-            mean(
-                [analyzer.individuals[individual].fitness for individual in generation],
-            )
-            for generation in analyzer.generations
-        ]
+            min_fitness = [
+                min(
+                    [
+                        analyzer.individuals[individual].fitness
+                        for individual in generation
+                    ],
+                )
+                for generation in analyzer.generations
+            ]
 
-        x = [i for i in range(len(analyzer.generations))]
+            mean_fitness = [
+                mean(
+                    [
+                        analyzer.individuals[individual].fitness
+                        for individual in generation
+                    ],
+                )
+                for generation in analyzer.generations
+            ]
+            max_fitnesses.append(max_fitness)
+            min_fitnesses.append(min_fitness)
+            mean_fitnesses.append(mean_fitness)
+
+    assert all(
+        len(max_fitnesses[0]) == len(x) for x in max_fitnesses
+    ), "Not all databases have an equal amount of generations."
+
+    mean_max_fitness = [mean(x) for x in list(map(list, zip(*max_fitnesses)))]
+    mean_min_fitness = [mean(x) for x in list(map(list, zip(*min_fitnesses)))]
+    mean_avg_fitness = [mean(x) for x in list(map(list, zip(*mean_fitnesses)))]
+
+    x = [i for i in range(len(mean_max_fitness))]
 
     fig, ax = plt.subplots()
     ax.plot(
         x,
-        max_fitness,
+        mean_max_fitness,
     )
     ax.plot(
         x,
-        min_fitness,
+        mean_min_fitness,
     )
     ax.plot(
         x,
-        mean_fitness,
+        mean_avg_fitness,
     )
     plt.show()
 

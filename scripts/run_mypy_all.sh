8a3f01c5748e34c987a0d0ac1b67fa614bec2aa9
Aart Stuurman <aartstuurman@hotmail.com>
2022-02-03 12:20:54 +0100

    Test to break mypy

--- scripts/run_mypy_all.sh
+++ scripts/run_mypy_all.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-../core/run_mypy.sh
-../genotypes/cppnwin/run_mypy.sh
-../envs/isaacgym/run_mypy.sh
+../core/run_mypy.sh && ../genotypes/cppnwin/run_mypy.sh && ../runners/isaacgym/run_mypy.sh
--- scripts/run_mypy_all_opensource.sh
+++ scripts/run_mypy_all_opensource.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-../core/run_mypy.sh
-../genotypes/cppnwin/run_mypy.sh
+../core/run_mypy.sh && ../genotypes/cppnwin/run_mypy.sh

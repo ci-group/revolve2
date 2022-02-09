8efa9b624cd18f1f34ae04e2f30d018921b2be2b
Aart Stuurman <aartstuurman@hotmail.com>
2022-02-09 18:57:18 +0100

    Fix package name

--- genotypes/cppnwin/setup.py
+++ genotypes/cppnwin/setup.py
@@ -5,13 +5,12 @@ import os.path
 revolve2_path = pathlib.Path(__file__).parent.parent.parent.resolve()
 
 setup(
-    name="revolve2-genotype-cppnwin",
+    name="revolve2-genotypes-cppnwin",
     version="0.0.0",
     description="CPPNWIN genotype for modular robots from Revolve2",
     author="Computational Intelligence Group Vrije Universiteit",
     url="https://github.com/ci-group/revolve2",
     packages=find_namespace_packages(),
-    package_data={"revolve2.genotypes.cppnwin": ["py.typed"]},
     install_requires=[
         f"revolve2-core @ file://{os.path.join(revolve2_path, 'core')}",
         "multineat",

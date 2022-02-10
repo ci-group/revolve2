438b4df72b2d229ea506aecfea734b8c277d76e8
Aart Stuurman <aartstuurman@hotmail.com>
2022-02-10 11:53:01 +0100

    Add dev_requirements.sh that installs everything in editable mode

---
+++ dev_requirements.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# See https://github.com/pypa/pip/issues/10216
+# for why it is at the time of writing not possible to create a dev_requirements.txt
+
+pip install -e ./namespace[dev]
+pip install -e ./serialization[dev]
+pip install -e ./object_controller[dev]
+pip install -e ./rpi_controller[dev]
+pip install -e ./core[dev]
+pip install -e ./genotypes/cppnwin[dev]
+pip install -e ./runners/isaacgym[dev]
+pip install -e ./core[dev]

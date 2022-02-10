aa1fd65741192bc7e2394f833a1516f1d1688c3f
surgura <aartstuurman@hotmail.com>
2022-02-10 17:27:03 +0000

    Deploying to gh-pages from @ ci-group/revolve2@8526af794f48473150f088667bd7fa30b5a369cc 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.0.0-alpha1',
+    VERSION: 'v0.1.0-alpha2-alpha2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',

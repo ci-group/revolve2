e679b9df7c38a01d305a49eaf82e2e31aab23d1d
Aart Stuurman <aartstuurman@hotmail.com>
2022-02-23 12:49:34 +0100

    Hotfix: Add missing dependency to serialization lib.

--- serialization/setup.py
+++ serialization/setup.py
@@ -13,6 +13,7 @@ setup(
     url="https://github.com/ci-group/revolve2",
     packages=find_namespace_packages(),
     package_data={"revolve2.serialization": ["py.typed"]},
+    install_requires=["typing_extensions==4.1.1"],
     extras_require={"dev": []},
     zip_safe=False,
 )

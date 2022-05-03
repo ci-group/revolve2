07fd36868d4926ef744d28b9eb587eceb2148367
Aart Stuurman <aartstuurman@hotmail.com>
2022-05-03 13:28:50 +0200

    Simplify physics runner interface.

--- core/revolve2/core/physics/running/_runner.py
+++ core/revolve2/core/physics/running/_runner.py
@@ -1,11 +1,25 @@
+"""
+Runner class
+"""
+
 from abc import ABC, abstractmethod
-from typing import List, Tuple
+from typing import List
 
 from ._batch import Batch
-from ._state import State
+from ._state import RunnerState
 
 
 class Runner(ABC):
+    """
+    Interface class for physics runners.
+    Running happens either in simulation or reality, depending on the implementation.
+    """
+
     @abstractmethod
-    async def run_batch(self, batch: Batch) -> List[Tuple[float, State]]:
+    async def run_batch(self, batch: Batch) -> List[RunnerState]:
+        """
+        Simulate the provided batch.
+
+        :return: List of simulation states in ascending order of time.
+        """
         pass

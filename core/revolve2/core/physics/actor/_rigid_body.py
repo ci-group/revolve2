d5e517ccd3204a210e90dc4973a11520ad7e9ab1
Aart Stuurman <aartstuurman@hotmail.com>
2022-05-04 12:14:47 +0200

    Implement rotation of rigid body collisions

--- core/revolve2/core/physics/actor/_rigid_body.py
+++ core/revolve2/core/physics/actor/_rigid_body.py
@@ -13,10 +13,10 @@ class RigidBody:
     name: str
     position: Vector3
     orientation: Quaternion
-    collisions: List[Collision] = field(default_factory=list, init=False)
-    visuals: List[Visual] = field(default_factory=list, init=False)
     static_friction: float
     dynamic_friction: float
+    collisions: List[Collision] = field(default_factory=list, init=False)
+    visuals: List[Visual] = field(default_factory=list, init=False)
 
     def mass(self) -> float:
         return sum(collision.mass for collision in self.collisions)
@@ -31,23 +31,43 @@ class RigidBody:
         com = self.center_of_mass()
         inertia = Matrix33()
 
-        # TODO this does not take into account Collision orientation
-
         for collision in self.collisions:
-            inertia[0][0] += collision.mass * (
-                (collision.bounding_box.y**2 + collision.bounding_box.z**2) / 12.0
-                + (collision.position.y - com.y) ** 2
+            # calculate inertia in local coordinates
+            local_inertia = Matrix33()
+            local_inertia[0][0] += (
+                collision.mass
+                * (collision.bounding_box.y**2 + collision.bounding_box.z**2)
+                / 12.0
+            )
+            local_inertia[1][1] += (
+                collision.mass
+                * (collision.bounding_box.x**2 + collision.bounding_box.z**2)
+                / 12.0
+            )
+            inertia[2][2] += (
+                collision.mass
+                * (collision.bounding_box.x**2 + collision.bounding_box.y**2)
+                / 12.0
+            )
+
+            # convert to global coordinates
+            translation = Matrix33()
+            translation[0][0] += collision.mass * (
+                (collision.position.y - com.y) ** 2
                 + (collision.position.z - com.z) ** 2
             )
-            inertia[1][1] += collision.mass * (
-                (collision.bounding_box.x**2 + collision.bounding_box.z**2) / 12.0
-                + (collision.position.x - com.x) ** 2
+            translation[1][1] += collision.mass * (
+                (collision.position.x - com.x) ** 2
                 + (collision.position.z - com.z) ** 2
             )
-            inertia[2][2] += collision.mass * (
-                (collision.bounding_box.x**2 + collision.bounding_box.y**2) / 12.0
-                + (collision.position.x - com.x) ** 2
+            translation[2][2] += collision.mass * (
+                (collision.position.x - com.x) ** 2
                 + (collision.position.y - com.y) ** 2
             )
+
+            global_inertia = local_inertia * collision.orientation + translation
+
+            # add to rigid body inertia tensor
+            inertia += global_inertia
 
         return inertia

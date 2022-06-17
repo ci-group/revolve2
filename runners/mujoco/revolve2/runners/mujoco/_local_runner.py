9250d7607ea73d8ce5cecc6fb004311e354818bb
Aart Stuurman <aartstuurman@hotmail.com>
2022-06-17 13:59:16 +0200

    Add lights and position and orientation of robots

--- runners/mujoco/revolve2/runners/mujoco/_local_runner.py
+++ runners/mujoco/revolve2/runners/mujoco/_local_runner.py
@@ -28,8 +28,20 @@ class LocalRunner(Runner):
         for env_index, env_descr in enumerate(batch.environments):
             env_mjcf = mjcf.RootElement(model="environment")
             env_mjcf.worldbody.add(
-                "geom", name="ground", type="plane", size=[10, 10, 1]
+                "geom",
+                name="ground",
+                type="plane",
+                size=[10, 10, 1],
+                rgba=[0.2, 0.2, 0.2, 1],
             )
+            env_mjcf.worldbody.add(
+                "light",
+                pos=[0, 0, 100],
+                ambient=[0.5, 0.5, 0.5],
+                directional=True,
+                castshadow=False,
+            )
+            env_mjcf.visual.headlight.active = 0
 
             for actor_index, posed_actor in enumerate(env_descr.actors):
                 urdf = physbot_to_urdf(
@@ -52,8 +64,19 @@ class LocalRunner(Runner):
 
                 attachment_frame = env_mjcf.attach(robot)
                 attachment_frame.add("freejoint")
+                attachment_frame.pos = [
+                    posed_actor.position.x,
+                    posed_actor.position.y,
+                    posed_actor.position.z,
+                ]
 
-            print(env_mjcf.to_xml_string())
+                attachment_frame.quat = [
+                    posed_actor.orientation.x,
+                    posed_actor.orientation.y,
+                    posed_actor.orientation.z,
+                    posed_actor.orientation.w,
+                ]
+
             model = mujoco.MjModel.from_xml_string(env_mjcf.to_xml_string())
             data = mujoco.MjData(model)
 

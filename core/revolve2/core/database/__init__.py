95416e2b32c1530a15c377915ffc5657b2662820
Aart Stuurman <aartstuurman@hotmail.com>
2022-07-06 17:49:59 +0200

    run sort-all

--- core/revolve2/core/database/__init__.py
+++ core/revolve2/core/database/__init__.py
@@ -3,8 +3,8 @@ from ._serializer import Serializer
 from ._sqlite import open_async_database_sqlite, open_database_sqlite
 
 __all__ = [
-    "open_database_sqlite",
-    "open_async_database_sqlite",
     "IncompatibleError",
     "Serializer",
+    "open_async_database_sqlite",
+    "open_database_sqlite",
 ]
--- sort_all/sort_all.sh
+++ sort_all/sort_all.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-dirs="./actor_controller ./core ./genotypes ./rpi_controller ./runners ./serialization"
+cd "$(dirname "$0")"
+dirs="../actor_controller ../core ../genotypes ../rpi_controller ../runners ../serialization"
 find $dirs -type f -name '__init__.py' -print0 | xargs -0 sort-all 

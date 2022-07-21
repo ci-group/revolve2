1ca545126db62e91557f77b5ab53b5614654882c
Aart Stuurman <aartstuurman@hotmail.com>
2022-07-21 18:27:12 +0200

    Move all code tools to codetools/. add pyflakes

---
+++ codetools/fix_all.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+set -e
+
+cd "$(dirname "$0")"
+
+echo "--------------"
+echo "mypy"
+echo "--------------"
+./mypy/check_all.sh
+
+echo "--------------"
+echo "pyflakes"
+echo "--------------"
+./pyflakes/check.sh
+
+echo "--------------"
+echo "sort-all"
+echo "--------------"
+./sort_all/fix.sh
+
+echo "--------------"
+echo "black"
+echo "--------------"
+./black/fix.sh
+
+echo "--------------"
+echo "isort"
+echo "--------------"
+./isort/fix.sh
---
+++ codetools/pyflakes/check.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+cd "$(dirname "$0")"
+
+pyflakes ../../actor_controller ../../core ../../examples ../../genotypes ../../rpi_controller ../../runners ../../serialization

b6ab623f24ea169e8b9e35a98b831f4b78544ddd
Aart Stuurman <aartstuurman@hotmail.com>
2022-07-22 18:16:16 +0200

    Fix rpi example

--- core/revolve2/core/physics/actor/_collision.py
+++ core/revolve2/core/physics/actor/_collision.py
@@ -5,8 +5,28 @@ from pyrr import Quaternion, Vector3
 
 @dataclass
 class Collision:
+    """A collision box."""
+
+    """Name of the collision."""
     name: str
+
+    """Position of the collision."""
     position: Vector3
+
+    """Orientation of the collision."""
     orientation: Quaternion
+
+    """
+    Mass of the collision.
+
+    This the absolute mass, irrespective of the size of the bounding box.
+    """
     mass: float
-    bounding_box: Vector3  # sizes of the lengths of the bounding box. not half.
+
+    """
+    Box describing the collision.
+
+    Sizes of the length of the bounding box.
+    Not half of the box.
+    """
+    bounding_box: Vector3

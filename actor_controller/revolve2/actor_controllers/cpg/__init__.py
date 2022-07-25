a1f04f0989314b2436b85d6345f3b643ec51f14b
Aart Stuurman <aartstuurman@hotmail.com>
2022-07-25 14:32:00 +0200

    Sort all

--- actor_controller/revolve2/actor_controllers/cpg/__init__.py
+++ actor_controller/revolve2/actor_controllers/cpg/__init__.py
@@ -1,4 +1,4 @@
 from ._cpg import CpgActorController
 from ._cpg_network_structure import Cpg, CpgNetworkStructure, CpgPair
 
-__all__ = ["CpgActorController", "Cpg", "CpgNetworkStructure", "CpgPair"]
+__all__ = ["Cpg", "CpgActorController", "CpgNetworkStructure", "CpgPair"]

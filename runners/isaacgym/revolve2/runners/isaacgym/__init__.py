e5d0b9bf0be67bf32447fc86e8376b07d3108ea7
Aart Stuurman <aartstuurman@hotmail.com>
2022-08-03 17:48:13 +0200

    Create default for Isaac Runner sim_params argument.

--- runners/isaacgym/revolve2/runners/isaacgym/__init__.py
+++ runners/isaacgym/revolve2/runners/isaacgym/__init__.py
@@ -1,6 +1,6 @@
 """Physics runner using the Isaac Gym simulator."""
 
-from ._local_runner import LocalRunner
+from ._local_runner import LocalRunner, default_sim_params
 from ._modular_robot_rerunner import ModularRobotRerunner
 
-__all__ = ["LocalRunner", "ModularRobotRerunner"]
+__all__ = ["LocalRunner", "ModularRobotRerunner", "default_sim_params"]

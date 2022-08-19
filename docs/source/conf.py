c04f72a29c9b5a4967c8aa71e0112455ecf585df
Aart Stuurman <aartstuurman@hotmail.com>
2022-08-19 14:11:05 +0200

    Bump version to 0.3.1 beta1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = (
 author = "Computational Intelligence Group, Vrije Universiteit Amsterdam & Contributors"
 
 # The full version, including alpha/beta/rc tags
-release = "v0.3.0-beta1"
+release = "v0.3.1-beta1"
 
 
 # -- General configuration ---------------------------------------------------

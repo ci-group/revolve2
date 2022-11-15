bdfa16566f70ff670b1956f1a76d1d3873d1b8a5
Aart Stuurman <aartstuurman@hotmail.com>
2022-11-15 12:39:43 +0100

    replace simple_optimization example by xor

--- examples/run_mypy.sh
+++ examples/run_mypy.sh
@@ -3,8 +3,6 @@
 cd "$(dirname "$0")"
 echo "examples/optimize_modular:" && \
 mypy optimize_modular && \
-echo "examples/simple_optimization:" && \
-mypy simple_optimization && \
 echo "examples/simulate_isaac:" && \
 mypy simulate_isaac && \
 echo "examples/simulate_mujoco:" && \

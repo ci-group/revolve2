6e484033997e437e0f6e899debbc4895d5f49f13
Aart Stuurman <aartstuurman@hotmail.com>
2023-01-24 17:02:08 +0100

    bump version to 0.3.8 (#212)
    Co-authored-by: Aart Stuurman <astuurman@hotmail.com>

--- serialization/setup.py
+++ serialization/setup.py
@@ -8,7 +8,7 @@ revolve2_path = pathlib.Path(__file__).parent.parent.resolve()
 
 setup(
     name="revolve2-serialization",
-    version="0.3.7-beta1",
+    version="0.3.8-beta1",
     description="Serialization classes and helper functions for revolve2",
     author="Computational Intelligence Group Vrije Universiteit",
     url="https://github.com/ci-group/revolve2",

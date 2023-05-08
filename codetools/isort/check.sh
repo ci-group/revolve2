e16a23e9b1a9a5c8986f40e72ab93698124a362d
Aart Stuurman <astuurman@hotmail.com>
2023-05-09 00:51:42 +0200

    Isort only sorts revolve library.

--- codetools/isort/check.sh
+++ codetools/isort/check.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"
 
-isort --check-only --diff --profile black ../..
+isort --check-only --diff --profile black ../../actor_controller ../../core ../../examples ../../genotypes ../../rpi_controller ../../runners ../../serialization ../../standard_resources

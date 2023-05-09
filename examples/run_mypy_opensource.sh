7e69702f6768fdbad1da9d90ec4155aaead15b39
Aart Stuurman <astuurman@hotmail.com>
2023-05-10 00:38:23 +0200

    update examples run mypy scripts

--- examples/run_mypy_opensource.sh
+++ examples/run_mypy_opensource.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
 cd "$(dirname "$0")"
-echo "examples/optimize_modular:" && \
-mypy optimize_modular && \
-echo "examples/simple_optimization:" && \
-mypy simple_optimization && \
+echo "examples/robot_bodybrain_ea:" && \
+mypy robot_bodybrain_ea && \
+echo "examples/robot_brain_cmaes:" && \
+mypy robot_brain_cmaes && \
+echo "examples/rpi_controller_remote:" && \
+mypy rpi_controller_remote && \
 echo "examples/simulate_isaac:" && \
 echo "isaacgym not open source. skipping.." && \
 echo "examples/simulate_mujoco:" && \
 mypy simulate_mujoco && \
-echo "examples/openai_es:" && \
-mypy openai_es && \
-echo "examples/rpi_controller_remote:" && \
-mypy rpi_controller_remote
+echo "examples/xor_de:" && \
+mypy xor_de && \
+echo "examples/xor_ea:" && \
+mypy xor_ea

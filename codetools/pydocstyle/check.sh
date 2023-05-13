dafcac524feeb08e358508d53fc463340b34115a
Aart Stuurman <astuurman@hotmail.com>
2023-05-14 00:57:04 +0200

    update codetools checkers

--- codetools/pydocstyle/check.sh
+++ codetools/pydocstyle/check.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"
 
-pydocstyle ../../actor_controller ../../core ../../examples ../../genotypes ../../rpi_controller ../../runners ../../serialization ../../standard_resources
+pydocstyle ../../actor_controller/revolve2 ../../core/revolve2 ../../examples ../../genotypes/cppnwin/revolve2 ../../rpi_controller/revolve2 ../../runners/isaacgym/revolve2 ../../runners/mujoco/revolve2 ../../serialization/revolve2 ../../standard_resources/revolve2

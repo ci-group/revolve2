5be2b095f21516fc36be1519e2a80e12ed3a86b6
Aart Stuurman <astuurman@hotmail.com>
2023-05-25 16:58:05 +0200

    bump version to 0.3.10

--- standard_resources/setup.py
+++ standard_resources/setup.py
@@ -9,7 +9,7 @@ revolve2_path = pathlib.Path(__file__).parent.parent.resolve()
 
 setup(
     name="revolve2-standard-resources",
-    version="0.3.9-beta1",
+    version="0.3.10-beta1",
     description="Standard resources such as environments and robots that can be used for testing and research.",
     author="Computational Intelligence Group Vrije Universiteit",
     url="https://github.com/ci-group/revolve2",

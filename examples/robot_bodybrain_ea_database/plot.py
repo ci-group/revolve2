2ab6273f18df036ffb9cfd958524528af121c9f7
Aart Stuurman <astuurman@hotmail.com>
2023-09-04 13:36:47 +0200

    plotting for bodybrain

---
+++ examples/robot_bodybrain_ea_database/plot.py
@@ -0,0 +1,105 @@
+"""
+Plot fitness over generations for all experiments, averaged.
+"""
+
+import config
+from revolve2.standard_resources.logging import setup_logging
+from revolve2.core.database import OpenMethod, open_database_sqlite
+import pandas
+from generation import Generation
+from individual import Individual
+from population import Population
+import matplotlib.pyplot as plt
+from sqlalchemy import select
+from experiment import Experiment
+
+
+def main() -> None:
+    """Run the program."""
+
+    setup_logging()
+
+    dbengine = open_database_sqlite(
+        config.DATABASE_FILE, open_method=OpenMethod.OPEN_IF_EXISTS
+    )
+
+    df = pandas.read_sql(
+        select(
+            Experiment.id.label("experiment_id"),
+            Generation.generation_index,
+            Individual.fitness,
+        )
+        .join_from(Experiment, Generation, Experiment.id == Generation.experiment_id)
+        .join_from(Generation, Population, Generation.population_id == Population.id)
+        .join_from(Population, Individual, Population.id == Individual.population_id),
+        dbengine,
+    )
+
+    agg_per_experiment_per_generation = (
+        df.groupby(["experiment_id", "generation_index"])
+        .agg({"fitness": ["max", "mean"]})
+        .reset_index()
+    )
+    agg_per_experiment_per_generation.columns = [
+        "experiment_id",
+        "generation_index",
+        "max_fitness",
+        "mean_fitness",
+    ]
+
+    agg_per_generation = (
+        agg_per_experiment_per_generation.groupby("generation_index")
+        .agg({"max_fitness": ["mean", "std"], "mean_fitness": ["mean", "std"]})
+        .reset_index()
+    )
+    agg_per_generation.columns = [
+        "generation_index",
+        "max_fitness_mean",
+        "max_fitness_std",
+        "mean_fitness_mean",
+        "mean_fitness_std",
+    ]
+
+    plt.figure()
+
+    # Plot max
+    plt.plot(
+        agg_per_generation["generation_index"],
+        agg_per_generation["max_fitness_mean"],
+        label="Max fitness",
+        color="b",
+    )
+    plt.fill_between(
+        agg_per_generation["generation_index"],
+        agg_per_generation["max_fitness_mean"] - agg_per_generation["max_fitness_std"],
+        agg_per_generation["max_fitness_mean"] + agg_per_generation["max_fitness_std"],
+        color="b",
+        alpha=0.2,
+    )
+
+    # Plot mean
+    plt.plot(
+        agg_per_generation["generation_index"],
+        agg_per_generation["mean_fitness_mean"],
+        label="Mean fitness",
+        color="r",
+    )
+    plt.fill_between(
+        agg_per_generation["generation_index"],
+        agg_per_generation["mean_fitness_mean"]
+        - agg_per_generation["mean_fitness_std"],
+        agg_per_generation["mean_fitness_mean"]
+        + agg_per_generation["mean_fitness_std"],
+        color="r",
+        alpha=0.2,
+    )
+
+    plt.xlabel("Generation index")
+    plt.ylabel("Fitness")
+    plt.title("Mean and max fitness across repetitions with std as shade")
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()

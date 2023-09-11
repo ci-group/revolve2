dd086108b005fe5041bfe4991840a0594897bbf9
Aart Stuurman <astuurman@hotmail.com>
2023-09-11 11:36:09 +0200

    Add missing parameters to create_batch_multiple_isolated_robots_standard that are present in create_batch_single_robot_standard

--- standard_resources/revolve2/standard_resources/simulation.py
+++ standard_resources/revolve2/standard_resources/simulation.py
@@ -49,6 +49,8 @@ def create_batch_multiple_isolated_robots_standard(
     robots: List[ModularRobot],
     terrains: List[Terrain],
     simulation_time: Optional[int] = STANDARD_SIMULATION_TIME,
+    sampling_frequency: float = STANDARD_SAMPLING_FREQUENCY,
+    simulation_timestep: float = STANDARD_SIMULATION_TIMESTEP,
     control_frequency: float = STANDARD_CONTROL_FREQUENCY,
 ) -> Batch:
     """
@@ -56,15 +58,17 @@ def create_batch_multiple_isolated_robots_standard(
 
     :param robots: The robots to simulate.
     :param terrains: The terrain to put each robot in.
-    :param simulation_time: How long to simulate for. The default argument is standard, but you can set a different value if you want to.
-    :param control_frequency: The frequency at which the control function is called. The argument is standard, but you can set a different value if you want to.
+    :param simulation_time: See `Batch` class.
+    :param sampling_frequency: See `Batch` class.
+    :param simulation_timestep: See `Batch` class.
+    :param control_frequency: See `Batch` class.
     :returns: The created batch, ready for simulation.
     """
     return create_batch_multiple_isolated_robots(
         robots=robots,
         terrains=terrains,
         simulation_time=simulation_time,
-        sampling_frequency=STANDARD_SAMPLING_FREQUENCY,
-        simulation_timestep=STANDARD_SIMULATION_TIMESTEP,
+        sampling_frequency=sampling_frequency,
+        simulation_timestep=simulation_timestep,
         control_frequency=control_frequency,
     )

666cbd3df8b1a04f4e75f5cce023d06dafaf77a9
Oliver <o.weissl@vu.nl>
2023-09-15 14:47:57 +0200

    278 student guide how to get started (#284)
    * updated installation for new release, added student tutorial
    
    * added script for students to install required packages

---
+++ student_install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# See https://github.com/pypa/pip/issues/10216
+# for why it is at the time of writing not possible to create a student_requirements.txt
+
+cd "$(dirname "$0")"
+
+pip install -e ./ci_group[dev] && \
+pip install -e ./simulators/mujoco[dev] && \
+pip install -e ./experimentation[dev] && \
+pip install -e ./modular_robot[dev] && \
+pip install -e ./simulation[dev] && \
+pip install -e ./actor_controller[dev] && \
+pip install -e ./serialization[dev] && \
+pip install -r ./examples/robot_bodybrain_ea_database/requirements.txt
+pip install -r ./examples/robot_brain_cmaes_database/requirements.txt
+pip install -r ./examples/simple_ea_xor_database/requirements.txt

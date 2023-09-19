2b4488ee72bf865f74a8aab30948882589b21072
Oliver <o.weissl@vu.nl>
2023-09-19 08:47:02 +0200

    Vetor2 implementation ready

--- simulation/revolve2/simulation/vector2/__init__.py
+++ simulation/revolve2/simulation/vector2/__init__.py
@@ -1,5 +1,5 @@
 """A custom Vector2 Class for revolve2."""
 from .vector2 import Vector2
-from .vector2aux import unit, index
+from .vector2aux import index, unit
 
-__all__ = ["Vector2", "unit", "index"]
+__all__ = ["Vector2", "index", "unit"]

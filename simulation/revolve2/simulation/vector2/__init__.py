b78c1878d773106abedcd34652c06585e479b3bb
Oliver <o.weissl@vu.nl>
2023-09-22 16:51:43 +0200

    minor adjustments

--- simulation/revolve2/simulation/vector2/__init__.py
+++ simulation/revolve2/simulation/vector2/__init__.py
@@ -1,5 +1,5 @@
 """A custom Vector2 Class for revolve2."""
 from .vector2 import Vector2
-from .vector2aux import index, unit
+from .vector2aux import unit
 
-__all__ = ["Vector2", "index", "unit"]
+__all__ = ["Vector2", "unit"]

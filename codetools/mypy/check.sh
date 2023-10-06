33a5db6c0ec77e80a96979781c81066baafb1dd7
Aart Stuurman <astuurman@hotmail.com>
2023-10-06 09:28:52 +0200

    WIP rearranging mypy

--- codetools/mypy/check.sh
+++ codetools/mypy/check.sh
@@ -4,10 +4,11 @@ set -e
 
 cd "$(dirname "$0")"
 
-../../ci_group/run_mypy.sh
-../../examples/run_mypy.sh
-../../experimentation/run_mypy.sh
-../../modular_robot/run_mypy.sh
-../../modular_robot_simulation/run_mypy.sh
-../../simulation/run_mypy.sh
-../../simulators/mujoco/run_mypy.sh
+packages=("ci_group/revolve2" "examples/custom_brain" "examples/custom_terrain" "examples/evaluate_multiple_interacting_robots" "examples/evaluate_multiple_isolated_robots" "examples/evaluate_single_robot" "examples/experiment_setup" "examples/robot_bodybrain_ea" "examples/robot_bodybrain_ea_database" "examples/robot_brain_cmaes" "examples/robot_brain_cmaes_database" "examples/simple_ea_xor" "examples/simple_ea_xor_database" "examples/simulate_single_robot" "experimentation/revolve2" "modular_robot/revolve2" "modular_robot_simulation/revolve2" "simulation/revolve2" "simulators/mujoco/revolve2")
+
+for package in "${packages[@]}"; do
+    echo "$package:"
+    mypy --config-file ./mypy.ini "../../$package"
+done
+
+#mypy --config-file ./mypy.ini ../../ci_group/revolve2

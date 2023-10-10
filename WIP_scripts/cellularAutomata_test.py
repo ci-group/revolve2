159d8520b4a61ac2b17154030e624d107551c05d
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-10-10 14:59:04 +0200

    reformat WIP scripts

--- WIP_scripts/cellularAutomata_test.py
+++ WIP_scripts/cellularAutomata_test.py
@@ -6,6 +6,7 @@ Just to test the update function without networks
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 # Function representing update rules for Cellular Automata
 def custom_update(grid, possible_values):
     dsize = grid.shape[0]
@@ -13,37 +14,60 @@ def custom_update(grid, possible_values):
 
     for i in range(1, dsize - 1):
         for j in range(1, dsize - 1):
-            inp = [grid[i][j], grid[i + 1][j], grid[i - 1][j],
-                   grid[i][j + 1], grid[i][j - 1]]
+            inp = [
+                grid[i][j],
+                grid[i + 1][j],
+                grid[i - 1][j],
+                grid[i][j + 1],
+                grid[i][j - 1],
+            ]
 
             # Apply custom update rules
             # You can define your own rules based on inp and possible_values
             # For example, let's assume a rule that averages neighboring values
             average_value = sum(inp) / len(inp)
-            domain_next[i][j] = min(possible_values, key=lambda x: abs(x - average_value))
+            domain_next[i][j] = min(
+                possible_values, key=lambda x: abs(x - average_value)
+            )
 
     return domain_next
 
-# Initialize the grid with random values from a specified list of possible values
-dsize = 20
-possible_values = [0, 1, 2, 3]  # Specify the possible values
-initial_grid = np.random.choice(possible_values, size=(dsize, dsize))
 
-# Number of iterations
-num_iterations = 10
+def main():
+    # Initialize the grid with random values from a specified list of possible values
+    dsize = 20
+    possible_values = [0, 1, 2, 3]  # Specify the possible values
+    initial_grid = np.random.choice(possible_values, size=(dsize, dsize))
 
-# Create a figure with subplots for visualization
-fig, axs = plt.subplots(1, num_iterations + 1, figsize=(15, 5))
+    # Number of iterations
+    num_iterations = 10
 
-# Plot the initial state
-axs[0].imshow(initial_grid, cmap='viridis', vmin=min(possible_values), vmax=max(possible_values))
-axs[0].set_title('Initial State')
+    # Create a figure with subplots for visualization
+    fig, axs = plt.subplots(1, num_iterations + 1, figsize=(15, 5))
 
-# Iterate and update the state for each iteration
-current_state = initial_grid
-for i in range(1, num_iterations + 1):
-    current_state = custom_update(current_state, possible_values)
-    axs[i].imshow(current_state, cmap='viridis', vmin=min(possible_values), vmax=max(possible_values))
-    axs[i].set_title(f'Iteration {i}')
+    # Plot the initial state
+    axs[0].imshow(
+        initial_grid,
+        cmap="viridis",
+        vmin=min(possible_values),
+        vmax=max(possible_values),
+    )
+    axs[0].set_title("Initial State")
 
-plt.show()
+    # Iterate and update the state for each iteration
+    current_state = initial_grid
+    for i in range(1, num_iterations + 1):
+        current_state = custom_update(current_state, possible_values)
+        axs[i].imshow(
+            current_state,
+            cmap="viridis",
+            vmin=min(possible_values),
+            vmax=max(possible_values),
+        )
+        axs[i].set_title(f"Iteration {i}")
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
--- WIP_scripts/neuralCellularAutomata_test.py
+++ WIP_scripts/neuralCellularAutomata_test.py
@@ -1,8 +1,8 @@
 """
 Prototype test for neural cellular automata representation
 
-Doesn't have life mask like in https://distill.pub/2020/growing-ca/ ... 
-I will add it if the model turns out to be useful 
+Doesn't have life mask like in https://distill.pub/2020/growing-ca/ ...
+I will add it if the model turns out to be useful
 
 ISSUE: The core script seem to work properly, but NN doesn't actually learn to recreate an image
        Although this is perhaps not the main point of the code
@@ -15,20 +15,22 @@ import torch
 from torch import nn
 import torch.optim as optim
 
+
 class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
         self.linear_relu_stack = nn.Sequential(
             nn.Linear(5, 32),
-            nn.Tanh(),       # tanh is used according to the paper on CA from Karine
-            nn.Linear(32, 3)  # number of output nodes = number of possible cell types
+            nn.Tanh(),  # tanh is used according to the paper on CA from Karine
+            nn.Linear(32, 3),  # number of output nodes = number of possible cell types
         )
 
     def forward(self, x):
         x = self.flatten(x)
         x = self.linear_relu_stack(x)
         return x
+
 
 # function to visualize the activation map
 def visualize_activation(grid, model):
@@ -37,14 +39,20 @@ def visualize_activation(grid, model):
 
     for i in range(1, dsize - 1):
         for j in range(1, dsize - 1):
-            inp = [grid[i][j], grid[i + 1][j], grid[i - 1][j],
-                   grid[i][j + 1], grid[i][j - 1]]
+            inp = [
+                grid[i][j],
+                grid[i + 1][j],
+                grid[i - 1][j],
+                grid[i][j + 1],
+                grid[i][j - 1],
+            ]
 
             inp_tensor = torch.tensor([inp], dtype=torch.float32, requires_grad=True)
             out = model(inp_tensor)
             activation_map[i][j] = out.detach().numpy()
 
     return activation_map
+
 
 # cellular automaton update function
 def update_grid(grid, model):
@@ -53,8 +61,13 @@ def update_grid(grid, model):
 
     for i in range(1, dsize - 1):
         for j in range(1, dsize - 1):
-            inp = [grid[i][j], grid[i + 1][j], grid[i - 1][j],
-                   grid[i][j + 1], grid[i][j - 1]]
+            inp = [
+                grid[i][j],
+                grid[i + 1][j],
+                grid[i - 1][j],
+                grid[i][j + 1],
+                grid[i][j - 1],
+            ]
 
             inp_tensor = torch.tensor([inp], dtype=torch.float32, requires_grad=True)
             out = model(inp_tensor)
@@ -62,29 +75,46 @@ def update_grid(grid, model):
 
     return domain_next
 
-if __name__ == '__main__':
+
+def main():
     # domain to be learned
     dsize = 10
-    target_domain = np.fromfunction(lambda x, y: ((x - dsize / 2)**2 + (y - dsize / 2)**2 <= (dsize / 4)**2).astype(int), (dsize, dsize))
-    start_domain = np.random.randint(3, size=(dsize, dsize))  # Random initial grid with 3 classes
+    target_domain = np.fromfunction(
+        lambda x, y: (
+            (x - dsize / 2) ** 2 + (y - dsize / 2) ** 2 <= (dsize / 4) ** 2
+        ).astype(int),
+        (dsize, dsize),
+    )
+    start_domain = np.random.randint(
+        3, size=(dsize, dsize)
+    )  # Random initial grid with 3 classes
 
     model = NeuralNetwork()
     optimizer = optim.SGD(model.parameters(), lr=0.01)
 
-    # training loop 
+    # training loop
     num_train_iter = 50
-    for j in range(num_train_iter):
+    for _ in range(num_train_iter):
         optimizer.zero_grad()
 
         # create domain based on rules from NN
-        for i in range(10):
-            domain_next = update_grid(start_domain, model)
+        domain_next = start_domain
+        for _ in range(10):
+            domain_next = update_grid(domain_next, model)
 
-        domain_tensor = torch.tensor(start_domain, dtype=torch.float32, requires_grad=True)
-        domain_next_tensor = torch.tensor(domain_next, dtype=torch.float32, requires_grad=True)
+        domain_tensor = torch.tensor(
+            start_domain, dtype=torch.float32, requires_grad=True
+        )
+        domain_next_tensor = torch.tensor(
+            domain_next, dtype=torch.float32, requires_grad=True
+        )
 
         # measure of how close generated image is to the original
-        similarity = torch.mean(torch.abs(domain_next_tensor - torch.tensor(target_domain, dtype=torch.float32)))
+        similarity = torch.mean(
+            torch.abs(
+                domain_next_tensor - torch.tensor(target_domain, dtype=torch.float32)
+            )
+        )
         loss = 1 - similarity
 
         loss.backward()
@@ -102,15 +132,19 @@ if __name__ == '__main__':
         activation_layers.append(activation_map)
 
     # plot the activation layers, the final grid, and the target domain for comparison
-    fig, axs = plt.subplots(1, num_iterations + 2, figsize=(15, 5))
+    _, axs = plt.subplots(1, num_iterations + 2, figsize=(15, 5))
     for i in range(num_iterations):
-        axs[i].imshow(activation_layers[i][:, :, 0], cmap='viridis')
-        axs[i].set_title(f'Iteration {i}')
+        axs[i].imshow(activation_layers[i][:, :, 0], cmap="viridis")
+        axs[i].set_title(f"Iteration {i}")
 
-    axs[num_iterations].imshow(domain, cmap='viridis')
-    axs[num_iterations].set_title('Final Grid')
+    axs[num_iterations].imshow(domain, cmap="viridis")
+    axs[num_iterations].set_title("Final Grid")
 
-    axs[num_iterations + 1].imshow(target_domain, cmap='viridis') 
-    axs[num_iterations + 1].set_title('Target Domain')
+    axs[num_iterations + 1].imshow(target_domain, cmap="viridis")
+    axs[num_iterations + 1].set_title("Target Domain")
 
     plt.show()
+
+
+if __name__ == "__main__":
+    main()

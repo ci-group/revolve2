6768ca372cf621ea4d308699aee1c82b9053436b
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-10-17 11:54:32 +0200

    Make render_robot assert its presumptions

--- modular_robot/revolve2/modular_robot/representations/__init__.py
+++ modular_robot/revolve2/modular_robot/representations/__init__.py
@@ -1,8 +1,12 @@
 from ._render import Render
-from revolve2.modular_robot import ModularRobot
+from revolve2.modular_robot import ModularRobot, MorphologicalMeasures
 
 from pathlib import Path
 
 
 def render_robot(robot: ModularRobot, path: Path) -> None:
-    Render().render_robot(robot.body.core, path)
+    body = robot.body
+    if not MorphologicalMeasures(body).is_2d:
+        raise ValueError("robot is not 2d")
+
+    Render().render_robot(body.core, path)

f77a56db74f9e1fdc5b2ce65eef5091aa6e92f18
Aart Stuurman <aartstuurman@hotmail.com>
2023-10-18 16:45:46 +0200

    CPG brain sets hinge targets based on their range instead of [-1, 1]
    * CPG brain sets hinge targets based on their range instead of [-1, 1].
    * Removed clipping from CPG.
    * Added clipping to modular robot control interface.

--- modular_robot/revolve2/modular_robot/body/v1/_active_hinge_v1.py
+++ modular_robot/revolve2/modular_robot/body/v1/_active_hinge_v1.py
@@ -24,7 +24,7 @@ class ActiveHingeV1(ActiveHinge):
             num_children=1,
             rotation=rotation,
             color=self._COLOR,
-            range=1.047197551,
+            range=1.047197551,  # 60 degrees
             effort=0.948013269,  # motor specs: 9.4 kgfcm at 4.8V or 11 kgfcm at 6.0V -> at 5.0V: 9.6667 * 9.807 / 100
             velocity=6.338968228,  # motor specs: 0.17  at 4.8V or 0.14 s/60deg at 6.0V -> at 5.0V: 1 / 0.1652 * 60 / 360 * 2pi
             frame_bounding_box=Vector3([0.018, 0.053, 0.0165891]),

efdd8341fe423ddc2fd6968e44a8c4f385a2a6cd
Oliver <o.weissl@vu.nl>
2023-10-19 18:59:50 +0200

    exec

---
+++ modular_robot_physical/revolve2/execute_controller.py
@@ -0,0 +1,10 @@
+from revolve2.modular_robot_physical import PhysicalRobotController
+
+
+def main() -> None:
+    """Run the script."""
+    PhysicalRobotController().main()
+
+
+if __name__ == "__main__":
+    main()

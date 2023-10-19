4b766193ca597d315254b02060ca0e1b7a250397
Oliver <o.weissl@vu.nl>
2023-10-19 19:23:55 +0200

    pls

--- modular_robot_physical/revolve2/modular_robot_physical/physical_robot_sensor_states/_physical_sensor_state.py
+++ modular_robot_physical/revolve2/modular_robot_physical/physical_robot_sensor_states/_physical_sensor_state.py
@@ -1,7 +1,11 @@
-from revolve2.modular_robot.sensor_state import ModularRobotSensorState
+from revolve2.modular_robot.body.base import ActiveHingeSensor
+from revolve2.modular_robot.sensor_state import ModularRobotSensorState, ActiveHingeSensorState
 
 
 class PhysicalSensorState(ModularRobotSensorState):
     """A Class for using physical sensors."""
 
-    pass
+    def get_active_hinge_sensor_state(
+        self, sensor: ActiveHingeSensor
+    ) -> ActiveHingeSensorState:
+        pass

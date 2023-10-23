3a926cf109967870eeacffa9fa3a45e67ed483eb
Oliver <o.weissl@vu.nl>
2023-10-23 18:25:47 +0200

    rm empty line

--- dev_install.sh
+++ dev_install.sh
@@ -17,4 +17,3 @@ pip install -r ./docs/requirements.txt && \
 pip install -r ./examples/robot_bodybrain_ea_database/requirements.txt && \
 pip install -r ./examples/robot_brain_cmaes_database/requirements.txt && \
 pip install -r ./examples/simple_ea_xor_database/requirements.txt
-

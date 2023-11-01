2f2802ba7c1ad1da49be7066461554f27f4497da
Aart Stuurman <aartstuurman@hotmail.com>
2023-11-01 15:06:11 +0100

    Implement stream brain cli

--- modular_robot_physical/revolve2/modular_robot_physical/_bin/stream_brain.py
+++ modular_robot_physical/revolve2/modular_robot_physical/_bin/stream_brain.py
@@ -1,7 +1,19 @@
-"""Run a brain on a physical robot."""
+"""
+Directly control a physical robot robot by providing commands over an input stream
+
+Commands:
+
+setpins:
+    {"cmd": "setpins", "pins": [{"pin": int, "target": float}]}
+    Sets the target for the provided pins.
+"""
 import typed_argparse as tap
 
 from ..physical_interfaces import HardwareType, PhysicalInterface, get_interface
+import sys
+import json
+from json.decoder import JSONDecodeError
+import traceback
 
 
 class Args(tap.TypedArgs):
@@ -11,6 +23,13 @@ class Args(tap.TypedArgs):
     debug: bool = tap.arg(help="Print debug information.")
     dry: bool = tap.arg(help="Skip GPIO output.")
     pins: list[int] = tap.arg(help="The GPIO pins that will be used.")
+    m2m: bool = tap.arg(
+        help="Enable machine-to-machine mode. Only print messages relevant to the protocol."
+    )
+
+
+class CommandError(Exception):
+    pass
 
 
 class Program:
@@ -19,7 +38,12 @@ class Program:
     _physical_interface: PhysicalInterface
 
     def run(
-        self, hardware_type: HardwareType, debug: bool, dry: bool, pins: list[int]
+        self,
+        hardware_type: HardwareType,
+        debug: bool,
+        dry: bool,
+        pins: list[int],
+        m2m: bool,
     ) -> None:
         """
         Run the program.
@@ -28,17 +52,41 @@ class Program:
         :param debug: If debugging messages are activated.
         :param dry: If servo outputs are not propagated to the physical servos.:
         :param pins: The GPIO pins that will be used.
+        :param m2m: Enable machine-to-machine mode. Only print messages relevant to the protocol.
         :raises RuntimeError: If shutdown was not clean.
         """
         try:
-            print("Exit the program at any time by pressing Ctrl-C.")
+            if not m2m:
+                print("Exit the program at any time by pressing Ctrl-C.")
 
             self._physical_interface = get_interface(
                 hardware_type=hardware_type, debug=debug, dry=dry, pins=pins
             )
 
-            while True:
-                pass
+            for line in sys.stdin:
+                try:
+                    parsed = json.loads(line)
+
+                    match parsed:
+                        case {"cmd": "setpins", "pins": list(pins_to_set)}:
+                            for pin in pins_to_set:
+                                match pin:
+                                    case {
+                                        "pin": int(pin),
+                                        "target": float(target),
+                                    }:
+                                        self._physical_interface.set_servo_target(
+                                            pin=pin, target=target
+                                        )
+                                    case _:
+                                        raise CommandError("Invalid command.")
+                        case _:
+                            raise CommandError("Invalid command.")
+                except (CommandError, JSONDecodeError) as e:
+                    if not m2m:
+                        print("Error parsing command:")
+                        print(traceback.format_exc())
+                        print("Recovered from error. You can send another command.")
 
         except KeyboardInterrupt:
             print("Program interrupted by user. Exiting..")
@@ -58,7 +106,11 @@ def runner(args: Args) -> None:
     :param args: The parsed program arguments.
     """
     Program().run(
-        hardware_type=args.hardware, debug=args.debug, dry=args.dry, pins=args.pins
+        hardware_type=args.hardware,
+        debug=args.debug,
+        dry=args.dry,
+        pins=args.pins,
+        m2m=args.m2m,
     )
 
 

0b7603f236f62f6c42c16085e824c43aa1ee9927
Oliver <o.weissl@vu.nl>
2023-11-10 14:49:25 +0100

    example usage in init

--- ci_group/revolve2/ci_group/morphological_novelty_metric/__init__.py
+++ ci_group/revolve2/ci_group/morphological_novelty_metric/__init__.py
@@ -1,4 +1,12 @@
-"""Calculate Morphological Novelty across a Population."""
+"""
+Calculate Morphological Novelty across a Population.
+
+Using the MorphologicalNoveltyMetric for evaluation of populations can be done as follows:
+
+ >>> robots: list[revolve2.modular_robot.ModularRobot]
+ >>> mnm = MorphologicalNoveltyMetric()
+ >>> novelties = mnm.get_novelty_from_population(robots)
+"""
 
 from ._morphological_novelty_metric import MorphologicalNoveltyMetric
 

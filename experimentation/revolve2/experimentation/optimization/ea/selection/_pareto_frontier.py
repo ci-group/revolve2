4ec25c4c637f0fb9a9397338c0101c34ee1a6201
Oliver <o.weissl@vu.nl>
2023-11-16 14:20:53 +0100

    upd

--- experimentation/revolve2/experimentation/optimization/ea/selection/_pareto_frontier.py
+++ experimentation/revolve2/experimentation/optimization/ea/selection/_pareto_frontier.py
@@ -17,9 +17,9 @@ def pareto_frontier(
     For mor information on the pareto frontier check: https://en.wikipedia.org/wiki/Pareto_front.
 
     :param frontier_values: Lists of values that are used for the frontier. The order of the list represents the importance of a value in descending order. These values need to be numeric.
-    :param frontier_order: List of orders for the values used in frontier selection. True = Descending, False = Ascending.
+    :param frontier_order: List of orders for the values used in frontier selection. True = ascending, False = descending.
     :param to_take: The amount of individuals to return from the frontier.
-    :returns: The index of the individuals that were selected.
+    :returns: The index of the individuals that were selected. Returned in descending order, wrt. frontier values and domination order (Best is first).
     """
     assert all(len(x) == len(y) for x, y in combinations(frontier_values, 2))
     assert len(frontier_values[0]) >= to_take
@@ -30,7 +30,7 @@ def pareto_frontier(
 
     all_values = np.array([domination_orders] + frontier_values, dtype=np.float64)
     frontier_order = [False] + frontier_order
-    """Domination order is Ascending by default. The more individuals are dominated by a target, the better."""
+    """Domination order is descending by default. The more individuals are dominated by a target, the better."""
 
     srt_array = [
         all_values[i] if frontier_order[i] else -all_values[i]
@@ -38,9 +38,7 @@ def pareto_frontier(
     ]
     srt = np.lexsort(srt_array[:-1])
     """Lexical sort needs reversed order, where the most important feature is sorted last."""
-
-    indices = np.arange(len(frontier_values[0]))[srt]
-    return list(indices[:to_take])
+    return list(srt[:to_take])
 
 
 def _get_domination_orders(

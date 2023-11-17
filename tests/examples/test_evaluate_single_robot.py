279a9c465f9088fc1e1364405e62c56194dbfd77
Oliver <o.weissl@vu.nl>
2023-11-17 15:02:10 +0100

    working unit tests

--- tests/examples/test_evaluate_single_robot.py
+++ tests/examples/test_evaluate_single_robot.py
@@ -6,4 +6,4 @@ from ..conftest import EXAMPLES_DIR, assert_command_succeeds
 def test_evaluate_single_robot() -> None:
     """Test evaluate_single_robot example can complete."""
     exp_dir = os.path.join(EXAMPLES_DIR, "evaluate_single_robot")
-    assert_command_succeeds(["python3", os.path.join(exp_dir, "main.py"), "-t", "1"])
+    assert_command_succeeds(["python", os.path.join(exp_dir, "main.py"), "-t", "1"])

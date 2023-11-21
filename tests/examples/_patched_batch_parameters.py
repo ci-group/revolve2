db369a920a2274aecf4065f92fbe658b4f770ddc
Oliver <o.weissl@vu.nl>
2023-11-21 16:10:54 +0100

    updated - something not working yet

---
+++ tests/examples/_patched_batch_parameters.py
@@ -0,0 +1,16 @@
+"""Standard simulation functions and parameters patched for unit test."""
+from revolve2.simulation.simulator import BatchParameters
+
+
+def make_patched_batch_parameters() -> BatchParameters:
+    """
+    Create batch parameters for unit-tests.
+
+    :returns: The create batch parameters.
+    """
+    return BatchParameters(
+        simulation_time=1,
+        sampling_frequency=5,
+        simulation_timestep=0.001,
+        control_frequency=20,
+    )
--- tests/examples/test_simulate_single_robot.py
+++ tests/examples/test_simulate_single_robot.py
@@ -1,14 +1,30 @@
 import os
+import sys
+from unittest.mock import Mock
 
-from ..conftest import EXAMPLES_DIR, assert_command_succeeds
+from ..conftest import EXAMPLES_DIR
+from ._clear_example_modules_from_cache import clear_exp_modules_from_cache
+from ._patched_batch_parameters import make_patched_batch_parameters
 
 
-def test_simulate_single_robot() -> None:
-    """Test simulate_single_robot example can complete."""
+def test_simulate_single_robot(mocker: Mock) -> None:
+    """
+    Test simulate_single_robot example can complete.
+
+    :param mocker: The mock object.
+    """
     exp_dir = os.path.join(EXAMPLES_DIR, "simulate_single_robot")
-    assert_command_succeeds(
-        [
-            "python",
-            os.path.join(exp_dir, "main.py"),
-        ]
-    )
+    # Clear any previously imported modules from examples directory from cache
+    clear_exp_modules_from_cache()
+    # Add examples directory to path, so we can import them without the examples being packages.
+    sys.path.insert(0, exp_dir)
+    # Override import for patching batch parameters.
+    mocker.patch("main.make_standard_batch_parameters", make_patched_batch_parameters)
+    try:
+        # This type ignore is required since mypy cant resolve this import.
+        import main  # type: ignore
+
+        main.main()
+    finally:
+        # Remove the example directory from path, even if the test fails.
+        sys.path.pop(0)

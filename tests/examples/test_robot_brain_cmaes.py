e3fc82898311e3cbb93247b56736b1f54ef75e19
Oliver <o.weissl@vu.nl>
2023-11-21 13:56:36 +0100

    upd

--- tests/examples/test_robot_brain_cmaes.py
+++ tests/examples/test_robot_brain_cmaes.py
@@ -3,7 +3,7 @@ import sys
 from unittest.mock import Mock
 
 from ..conftest import EXAMPLES_DIR
-from ._clear_example_modules_from_cache import clear_example_modules_from_cache
+from ._clear_example_modules_from_cache import clear_exp_modules_from_cache
 
 
 def test_robot_brain_cmaes(mocker: Mock) -> None:
@@ -12,22 +12,21 @@ def test_robot_brain_cmaes(mocker: Mock) -> None:
 
     :param mocker: The mock object.
     """
-    example_dir = os.path.join(EXAMPLES_DIR, "robot_brain_cmaes")
+    exp_dir = os.path.join(EXAMPLES_DIR, "robot_brain_cmaes")
 
     # Clear any previously imported modules from examples directory from cache
-    clear_example_modules_from_cache()
+    clear_exp_modules_from_cache()
 
     # Add examples directory to path, so we can import them without the examples being packages.
-    sys.path.insert(0, example_dir)  # Add the example directory to sys.path
+    sys.path.insert(0, exp_dir)  # Add the example directory to sys.path
 
     # Override default config to reduce number of generations.
     mocker.patch("config.NUM_GENERATIONS", 2)
 
     # Import the example main and run it.
     try:
+        # This type ignore is required since mypy cant resolve this import.
         import main  # type: ignore
-
-        """This type ignore is required since mypy cant resolve this import."""
 
         main.main()
     finally:

71e48fb66966c2ee42a1edfcd2309df459c9fc19
Aart Stuurman <aartstuurman@hotmail.com>
2023-12-18 15:03:16 +0100

    Publish Revolve2 on PyPI (#397)

---
+++ uninstall.sh
@@ -0,0 +1,13 @@
+# pip uninstall for every package which name contains 'revolve2'
+packages=$(pip list | grep revolve2 | awk '{print $1}')
+if [ -n "$packages" ]; then
+    echo "The following packages will be uninstalled:"
+    echo "$packages"
+    read -p "Are you sure? (y/N) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        pip uninstall $packages -y
+    fi
+else
+    echo "No packages to uninstall."
+fi

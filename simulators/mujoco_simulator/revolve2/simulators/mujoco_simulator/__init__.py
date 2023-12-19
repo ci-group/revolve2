21e969f084c337b229aa297c1938bf93d2daf57d
Oliver <o.weissl@vu.nl>
2023-12-19 14:33:14 +0100

    upd init

--- simulators/mujoco_simulator/revolve2/simulators/mujoco_simulator/__init__.py
+++ simulators/mujoco_simulator/revolve2/simulators/mujoco_simulator/__init__.py
@@ -1,6 +1,5 @@
 """Physics simulator using the MuJoCo."""
 
-from ._custom_mujoco_viewer import CustomMujocoViewer
 from ._local_simulator import LocalSimulator
 
-__all__ = ["CustomMujocoViewer", "LocalSimulator"]
+__all__ = ["LocalSimulator"]

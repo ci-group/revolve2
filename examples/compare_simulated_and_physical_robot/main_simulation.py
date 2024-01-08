e18830d5444ca87772eb097a548bbbb1bb9d4665
Oliver <o.weissl@vu.nl>
2024-01-08 11:33:34 +0100

    split physical / sim part

--- examples/compare_simulated_and_physical_robot/main_simulation.py
+++ examples/compare_simulated_and_physical_robot/main_simulation.py
@@ -1,4 +1,4 @@
-"""Compare physical robot with simulation behavior to validate the build."""
+"""Simulate the modular robot for comparison with the physical counterpart."""
 from revolve2.ci_group import terrains
 from revolve2.ci_group.modular_robots_v2 import gecko_v2
 from revolve2.ci_group.simulation import make_standard_batch_parameters
@@ -17,12 +17,6 @@ def main() -> None:
     body = gecko_v2()
     brain = BrainCpgNetworkNeighborRandom(body=body, rng=rng)
     robot = ModularRobot(body=body, brain=brain)
-
-    """Here we prepare the config for the physical twin of our Robot."""
-    # hinges = body.find_modules_of_type(ActiveHinge)
-    # pins = [6, 12, 13, 16, 17, 21]
-    # hinge_mapping = {UUIDKey(hinge): pin for hinge, pin in zip(hinges, pins)}
-    # TODO: make the physical counterpart
 
     """Now we create a scene with our simulated robot, to validate our physical robot."""
     simulator = LocalSimulator(manual_control=True)

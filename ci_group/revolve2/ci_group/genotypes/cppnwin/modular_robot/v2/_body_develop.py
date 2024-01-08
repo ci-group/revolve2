81cda3e1004d98193d749fe1d435f6335c7aff94
Oliver Weissl <o.weissl@vu.nl>
2024-01-08 11:14:29 +0100

    410 develop function bug (#411)

--- ci_group/revolve2/ci_group/genotypes/cppnwin/modular_robot/v2/_body_develop.py
+++ ci_group/revolve2/ci_group/genotypes/cppnwin/modular_robot/v2/_body_develop.py
@@ -37,24 +37,29 @@ def develop(
     genotype.BuildPhenotype(body_net)
 
     to_explore: Queue[__Module] = Queue()
-    grid = np.zeros(shape=(max_parts, max_parts, max_parts), dtype=np.uint8)
+    grid = np.zeros(
+        shape=(max_parts * 2 + 1, max_parts * 2 + 1, max_parts * 2 + 1), dtype=np.uint8
+    )
 
     body = BodyV2()
 
     v2_core = body.core_v2
+    core_position = Vector3(
+        [max_parts + 1, max_parts + 1, max_parts + 1], dtype=np.int_
+    )
+    grid[tuple(core_position)] = 1
+    part_count = 1
 
     for attachment_face in v2_core.attachment_faces.values():
         to_explore.put(
             __Module(
-                Vector3([0, 0, 0]),
+                core_position,
                 Vector3([0, -1, 0]),
                 Vector3([0, 0, 1]),
                 0,
                 attachment_face,
             )
         )
-    grid[0, 0, 0] = 1
-    part_count = 1
 
     while not to_explore.empty():
         module = to_explore.get()
@@ -79,7 +84,7 @@ def __evaluate_cppn(
     :param body_net: The CPPN network.
     :param position: Position of the module.
     :param chain_length: Tree distance of the module from the core.
-    :returns: (module type, orientation)
+    :returns: (module type, rotation_index)
     """
     x, y, z = position
     assert isinstance(
@@ -90,16 +95,15 @@ def __evaluate_cppn(
     outputs = body_net.Output()
 
     # get module type from output probabilities
-    type_probs = [outputs[0], outputs[1], outputs[2]]
+    type_probs = list(outputs[:3])
     types = [None, BrickV2, ActiveHingeV2]
     module_type = types[type_probs.index(min(type_probs))]
 
     # get rotation from output probabilities
-    rotation_probs = [outputs[3], outputs[4]]
-    rotation = rotation_probs.index(min(rotation_probs))
+    rotation_probs = list(outputs[3:5])
+    rotation_index = rotation_probs.index(min(rotation_probs))
 
-    # get attachment position
-    return (module_type, rotation)
+    return module_type, rotation_index
 
 
 def __add_child(
@@ -111,25 +115,25 @@ def __add_child(
     attachment_index, attachment_point = attachment_point_tuple
 
     forward = __rotate(module.forward, module.up, attachment_point.orientation)
-    position = module.position + forward
+    position = __vec3_int(module.position + forward)
     chain_length = module.chain_length + 1
 
     # if grid cell is occupied, don't make a child
     # else, set cell as occupied
-    grid_pos = np.round(position)
-    if grid[tuple(grid_pos)] > 0:
+    if grid[tuple(position)] > 0:
         return None
-    grid[grid_pos] += 1
+    grid[tuple(position)] += 1
 
     new_pos = np.array(np.round(position + attachment_point.offset), dtype=np.int64)
     child_type, child_rotation = __evaluate_cppn(body_net, new_pos, chain_length)
-    child_orientation = __make_quaternion_from_index(child_rotation)
+    angle = child_rotation * (np.pi / 2.0)
+    child = child_type(angle)
     if child_type is None or not module.module_reference.can_set_child(
-        child := child_type(child_orientation.angle), attachment_index
+        child, attachment_index
     ):
         return None
 
-    up = __rotate(module.up, forward, child_orientation)
+    up = __rotate(module.up, forward, Quaternion.from_eulers([angle, 0, 0]))
     module.module_reference.set_child(child, attachment_index)
 
     return __Module(
@@ -147,20 +151,24 @@ def __rotate(a: Vector3, b: Vector3, rotation: Quaternion) -> Vector3:
 
     :param a: Vector a.
     :param b: Vector b.
-    :param rotation: The rotation quaternion.
+    :param rotation: The quaternion for rotation.
     :returns: A copy of a, rotated.
     """
-    cosangle = np.cos(rotation.angle)
-    sinangle = np.sin(rotation.angle)
-    x, y, z = (a * cosangle + b.cross(a) * sinangle) + b * (b.dot(a) * (1 - cosangle))
-    return Vector3(np.array([x, y, z], dtype=np.int64))
+    cos_angle: int = int(round(np.cos(rotation.angle)))
+    sin_angle: int = int(round(np.sin(rotation.angle)))
+
+    vec: Vector3 = (
+        a * cos_angle + sin_angle * b.cross(a) + (1 - cos_angle) * b.dot(a) * b
+    )
+    return vec
 
 
-def __make_quaternion_from_index(index: int) -> Quaternion:
+def __vec3_int(vector: Vector3) -> Vector3[np.int_]:
     """
-    Make a quaternion from a rotation index.
+    Cast a Vector3 object to an integer only Vector3.
 
-    :param index: The index of rotation.
-    :return: The Quaternion.
+    :param vector: The vector.
+    :return: The integer vector.
     """
-    return Quaternion.from_x_rotation((np.pi / 2) * index)
+    x, y, z = map(lambda v: int(round(v)), vector)
+    return Vector3([x, y, z], dtype=np.int64)

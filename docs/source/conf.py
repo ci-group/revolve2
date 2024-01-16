b345df58e4bdaa532bb78558b5286512b2c016e2
Aart Stuurman <aartstuurman@hotmail.com>
2024-01-16 13:39:29 +0100

    version 1.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ copyright = (
     "Computational Intelligence Group, Vrije Universiteit Amsterdam & Contributors"
 )
 author = "Computational Intelligence Group, Vrije Universiteit Amsterdam & Contributors"
-release = "1.0.0"
+release = "1.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

f22f05d99d2711c8446f61f435e45b8219204b21
Aart Stuurman <aartstuurman@hotmail.com>
2024-01-16 13:36:59 +0100

    fix student install (#423)

--- student_install.sh
+++ student_install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Installs all Revolve2 packages in editable mode as well as all example requirements.
-# This is the same as installing `editable_requirements.txt`.
+# This is the same as installing `requirements_editable.txt`.
 
 cd "$(dirname "$0")"
 
-pip install -r ./editable_requirements.txt
+pip install -r ./requirements_editable.txt

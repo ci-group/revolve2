4e4d0568fe678db669dbc18650e1184d5781b34b
Oliver <o.weissl@vu.nl>
2024-01-20 10:33:46 +0100

    cast to float

--- modular_robot/revolve2/modular_robot/brain/cpg/_brain_cpg_instance_environmental_control.py
+++ modular_robot/revolve2/modular_robot/brain/cpg/_brain_cpg_instance_environmental_control.py
@@ -52,7 +52,7 @@ class BrainCpgInstanceEnvironmentalControl(BrainCpgInstance):
         # Set active hinge targets to match newly calculated state.
         for state_index, active_hinge in self._output_mapping:
             control_interface.set_active_hinge_target(
-                active_hinge, (float(self._state[state_index]) * active_hinge.range) * g
+                active_hinge, float((float(self._state[state_index]) * active_hinge.range) * g)
             )
 
 

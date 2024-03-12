cefb86831794bd22d9b742a98e2444bf783a17fc
oliver weißl <oliver.weissl@outlook.com>
2024-03-12 15:06:45 +0100

    upd

--- simulation/revolve2/simulation/scene/sensors/_camera_sensor.py
+++ simulation/revolve2/simulation/scene/sensors/_camera_sensor.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass, field
 
 from .._pose import Pose
-from ..vector2 import Vector2
 from ._sensor import Sensor
 
 
@@ -10,6 +9,6 @@ class CameraSensor(Sensor):
     """Camera sensor."""
 
     pose: Pose
-    camera_size: Vector2
+    camera_size: tuple[int, int]
     """Pose of the geometry, relative to its parent rigid body."""
     type: str = field(default="camera")

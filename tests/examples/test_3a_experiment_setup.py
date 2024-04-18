cb8c42ed25e46b259c3a00483991ca35e5c1e18d
Oliver Weissl <o.weissl@vu.nl>
2024-04-18 14:19:52 +0200

    Adjust Unit-Test Names(#544)

--- tests/examples/test_3a_experiment_setup.py
+++ tests/examples/test_3a_experiment_setup.py
@@ -3,7 +3,7 @@ import os
 from ..conftest import EXAMPLES_DIR, assert_command_succeeds
 
 
-def test_experiment_setup() -> None:
+def test_3a_experiment_setup() -> None:
     """Test 3a_experiment_setup example can complete."""
     exp_dir = os.path.join(EXAMPLES_DIR, "3_experiment_foundations/3a_experiment_setup")
     assert_command_succeeds(["python", os.path.join(exp_dir, "main.py")])
--- tests/examples/test_3b_evaluate_single_robot.py
+++ tests/examples/test_3b_evaluate_single_robot.py
@@ -7,13 +7,15 @@ from ._clear_example_modules_from_cache import clear_exp_modules_from_cache
 from ._patched_batch_parameters import make_patched_batch_parameters
 
 
-def test_simulate_single_robot(mocker: Mock) -> None:
+def test_3b_evaluate_single_robot(mocker: Mock) -> None:
     """
-    Test 1a_simulate_single_robot example can complete.
+    Test 3b_evaluate_single_robot example can complete.
 
     :param mocker: The mock object.
     """
-    exp_dir = os.path.join(EXAMPLES_DIR, "1_simulator_basics/1a_simulate_single_robot")
+    exp_dir = os.path.join(
+        EXAMPLES_DIR, "3_experiment_foundations/3b_evaluate_single_robot"
+    )
     # Clear any previously imported modules from examples directory from cache
     clear_exp_modules_from_cache()
     # Add examples directory to path, so we can import them without the examples being packages.

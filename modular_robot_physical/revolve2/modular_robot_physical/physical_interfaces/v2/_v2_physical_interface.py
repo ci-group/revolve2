9b69410b78221b948ff9491a28c8758ff43b8fad
oliverweissl <o.weissl@student.vu.nl>
2024-04-18 13:35:26 +0200

    upd

--- modular_robot_physical/revolve2/modular_robot_physical/physical_interfaces/v2/_v2_physical_interface.py
+++ modular_robot_physical/revolve2/modular_robot_physical/physical_interfaces/v2/_v2_physical_interface.py
@@ -9,6 +9,7 @@ from robohatlib.hal.assemblyboard.PwmPlug import PwmPlug
 from robohatlib.hal.assemblyboard.servo.ServoData import ServoData
 from robohatlib.hal.assemblyboard.ServoAssemblyConfig import ServoAssemblyConfig
 from robohatlib.Robohat import Robohat
+import cv2
 
 from .._physical_interface import PhysicalInterface
 

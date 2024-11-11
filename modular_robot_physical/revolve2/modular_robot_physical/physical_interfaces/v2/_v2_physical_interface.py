5094b420bdf52f9e86eed4b7d2ff1cc3667c6f2c
Ting-Chia Chiang <t.chiang@vu.nl>
2024-11-11 12:42:15 +0100

    Reformat

--- modular_robot_physical/revolve2/modular_robot_physical/physical_interfaces/v2/_v2_physical_interface.py
+++ modular_robot_physical/revolve2/modular_robot_physical/physical_interfaces/v2/_v2_physical_interface.py
@@ -4,7 +4,6 @@ from typing import Optional, Sequence
 import numpy as np
 from numpy.typing import NDArray
 from pyrr import Vector3
-
 from robohatlib.hal.assemblyboard.PwmPlug import PwmPlug
 from robohatlib.hal.assemblyboard.servo.ServoData import ServoData
 from robohatlib.hal.assemblyboard.ServoAssemblyConfig import ServoAssemblyConfig

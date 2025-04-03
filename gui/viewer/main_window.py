a7c49195f73b0c6b6305e3eae8836b25ff6aa31c
A-lamo <a.r.ferencz@student.vu.nl>
2025-04-03 13:53:40 +0200

    added more little tips

--- gui/viewer/main_window.py
+++ gui/viewer/main_window.py
@@ -366,7 +366,7 @@ class RobotEvolutionGUI(QMainWindow):
 
             if key == "FITNESS_EVAL_DURATION":
                 comment_label = QLabel("(seconds)")
-                comment_label.setToolTip("This is how long a single robot will be evaluated for.")
+                input_label.setToolTip("This is how long a single robot will be evaluated for.")
                 input_layout.addWidget(input_label)
                 input_layout.addWidget(input_field)
                 input_layout.addWidget(comment_label)
@@ -390,6 +390,7 @@ class RobotEvolutionGUI(QMainWindow):
             
             if key == "FITNESS_EVAL_DURATION":
                 comment_label = QLabel("(seconds)")
+                input_label.setToolTip("This is how long a single robot will be evaluated for.")
                 input_layout.addWidget(input_label)
                 input_layout.addWidget(input_field)
                 input_layout.addWidget(comment_label)

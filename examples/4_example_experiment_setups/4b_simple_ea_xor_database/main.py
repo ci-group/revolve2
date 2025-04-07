ededdd5a9198d7c03ece94a6319e6a9b08d1d4b9
andresgr96 <andresgarciar96@gmail.com>
2025-04-07 14:24:34 +0200

    fixed a bug in example 4b

--- examples/4_example_experiment_setups/4b_simple_ea_xor_database/main.py
+++ examples/4_example_experiment_setups/4b_simple_ea_xor_database/main.py
@@ -91,8 +91,8 @@ class SurvivorSelector(Selector):
         original_survivors, offspring_survivors = population_management.steady_state(
             [i.genotype for i in population.individuals],
             [i.fitness for i in population.individuals],
-            [i.genotype for i in offspring],
-            [i.fitness for i in offspring],
+            [i.genotype for i in offspring.individuals],
+            [i.fitness for i in offspring.individuals],
             lambda n, genotypes, fitnesses: selection.multiple_unique(
                 n,
                 genotypes,
@@ -112,8 +112,8 @@ class SurvivorSelector(Selector):
                 ]
                 + [
                     Individual(
-                        genotype=offspring[i].genotype,
-                        fitness=offspring[i].fitness,
+                        genotype=offspring.individuals[i].genotype,
+                        fitness=offspring.individuals[i].fitness,
                     )
                     for i in offspring_survivors
                 ]
@@ -147,8 +147,8 @@ class CrossoverReproducer(Reproducer):
             raise KeyError("No children passed.")
         offspring = [
             Genotype.crossover(
-                parents[parent1_i].genotype,
-                parents[parent2_i].genotype,
+                parents.individuals[parent1_i].genotype,
+                parents.individuals[parent2_i].genotype,
                 self._rng,
                 num_parameters=config.NUM_PARAMETERS,
             ).mutate(
